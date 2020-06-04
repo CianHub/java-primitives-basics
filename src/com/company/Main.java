128bdc5d9715c03045dd648fa8e85e0cd5f23628
Cian O'Gralaigh <ihatecian@gmail.com>
2020-06-04 20:50:34 +0100

    Experiementing with shorts and longs

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -5,13 +5,24 @@ public class Main {
     public static void main(String[] args) {
 	    int myValue = 10000;
 
+	    // 32bits
 	    int myMinIntValue = Integer.MIN_VALUE;
 	    int myMaxIntValue = Integer.MAX_VALUE;
 	    System.out.println("Min = " + myMinIntValue + " Max = " + myMaxIntValue);
 
+	    // 8bits
 	    byte myMinByteValue = Byte.MIN_VALUE;
 	    byte myMaxByteValue = Byte.MAX_VALUE;
 		System.out.println("Min = " + myMinByteValue + " Max = " + myMaxByteValue);
+
+		// 16bits
+		short myMinShortValue = Short.MIN_VALUE;
+		short myMaxShortValue = Short.MAX_VALUE;
+		System.out.println("Min = " + myMinShortValue + " Max = " + myMaxShortValue);
+
+		long myMinLongValue = Long.MIN_VALUE;
+		long myMaxLongValue = Long.MAX_VALUE;
+		System.out.println("Min = " + myMinLongValue + " Max = " + myMaxLongValue);
 
 	}
 }
