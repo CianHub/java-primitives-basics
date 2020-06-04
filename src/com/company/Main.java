53ebf0c572e32da0edfc4ec9eaae175ae5a9083f
Cian O'Gralaigh <ihatecian@gmail.com>
2020-06-04 20:45:36 +0100

    Experiementing with integers and bytes

---
+++ src/com/company/Main.java
@@ -0,0 +1,17 @@
+package com.company;
+
+public class Main {
+
+    public static void main(String[] args) {
+	    int myValue = 10000;
+
+	    int myMinIntValue = Integer.MIN_VALUE;
+	    int myMaxIntValue = Integer.MAX_VALUE;
+	    System.out.println("Min = " + myMinIntValue + " Max = " + myMaxIntValue);
+
+	    byte myMinByteValue = Byte.MIN_VALUE;
+	    byte myMaxByteValue = Byte.MAX_VALUE;
+		System.out.println("Min = " + myMinByteValue + " Max = " + myMaxByteValue);
+
+	}
+}
